015a91fa65c3e51185352c34ea35c49a1fa6d7f9
Your Name <you@example.com>
2024-05-10 21:52:33 -0700

    IBundler imported from /account instead of /bundler

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 import { DEFAULT_ENTRYPOINT_ADDRESS } from "@biconomy/account";
-import { IBundler, Bundler } from "@biconomy/bundler";
+import { IBundler, Bundler } from "@biconomy/account";
 import { ChainId } from "@biconomy/core-types";
 import { BiconomyPaymaster, IPaymaster } from "@biconomy/paymaster";
 
