a4decbb6f67bfe940c7f64161713df14e5c2caf5
Your Name <you@example.com>
2024-05-13 22:19:48 -0700

    Importing ethers, contants, paymaster and sponsored transaction; transfer function

--- src/components/Transfer.tsx
+++ src/components/Transfer.tsx
@@ -1,4 +1,11 @@
+import { USDC_CONTRACT_ADDRESS, ERC20ABI } from "@/constants";
 import { BiconomySmartAccountV2 } from "@biconomy/account";
+import {
+    IHybridPaymaster,
+    PaymasterMode,
+    SponsorUserOperationDto,
+  } from "@biconomy/paymaster";
+import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 
 export default function Transfer({
@@ -14,6 +21,105 @@ export default function Transfer({
   async function getSmartContractAddress() {
     const smartContractAddress = await smartAccount.getAccountAddress();
     setSmartContractAddress(smartContractAddress);
+  }
+
+  async function transfer() {
+    try {
+      // Initiate the loading state
+      setIsLoading(true);
+  
+      // Create an Ethers Contract instance for USDC
+      const readProvider = smartAccount.provider;
+      const tokenContract = new ethers.Contract(
+        USDC_CONTRACT_ADDRESS,
+        ERC20ABI,
+        readProvider
+      );
+  
+      // Fetch the amount of decimals in this ERC20 Contract
+      const decimals = await tokenContract.decimals();
+      // Convert the user inputted amount to the proper denomination unit based on the token decimals
+      const amountInLowestUnit = ethers.utils.parseUnits(
+        amount.toString(),
+        decimals
+      );
+  
+      // Create the calldata for our UserOperation
+      const populatedTransferTxn =
+        await tokenContract.populateTransaction.transfer(
+          recipient,
+          amountInLowestUnit
+        );
+      const calldata = populatedTransferTxn.data;
+  
+      // Build the UserOperation
+      const userOp = await smartAccount.buildUserOp([
+        {
+          to: USDC_CONTRACT_ADDRESS,
+          data: calldata,
+        },
+      ]);
+  
+      // Get the paymaster fee quote from Biconomy
+      const biconomyPaymaster =
+        smartAccount.paymaster as IHybridPaymaster<SponsorUserOperationDto>;
+      const feeQuoteResponse =
+        await biconomyPaymaster.getPaymasterFeeQuotesOrData(userOp, {
+          mode: PaymasterMode.ERC20,
+          tokenList: [],
+          preferredToken: USDC_CONTRACT_ADDRESS,
+        });
+      const feeQuote = feeQuoteResponse.feeQuotes;
+      if (!feeQuote) throw new Error("Could not fetch fee quote in USDC");
+  
+      const spender = feeQuoteResponse.tokenPaymasterAddress || "";
+      const selectedFeeQuote = feeQuote[0];
+  
+      // Build the paymaster userOp
+      let finalUserOp = await smartAccount.buildTokenPaymasterUserOp(userOp, {
+        feeQuote: selectedFeeQuote,
+        spender: spender,
+        maxApproval: true,
+      });
+  
+      // Get the calldata for the paymaster
+      const paymasterServiceData = {
+        mode: PaymasterMode.ERC20,
+        feeTokenAddress: USDC_CONTRACT_ADDRESS,
+        calculateGasLimits: true,
+      };
+      const paymasterAndDataResponse =
+        await biconomyPaymaster.getPaymasterAndData(
+          finalUserOp,
+          paymasterServiceData
+        );
+      finalUserOp.paymasterAndData = paymasterAndDataResponse.paymasterAndData;
+      
+      if (
+          paymasterAndDataResponse.callGasLimit &&
+          paymasterAndDataResponse.verificationGasLimit &&
+          paymasterAndDataResponse.preVerificationGas
+     ) {
+          // Returned gas limits must be replaced in your op as you update paymasterAndData.
+          // Because these are the limits paymaster service signed on to generate paymasterAndData
+  
+          finalUserOp.callGasLimit = paymasterAndDataResponse.callGasLimit;
+          finalUserOp.verificationGasLimit =
+            paymasterAndDataResponse.verificationGasLimit;
+          finalUserOp.preVerificationGas =
+            paymasterAndDataResponse.preVerificationGas;
+       }
+      // Send the UserOperation
+      const userOpResponse = await smartAccount.sendUserOp(finalUserOp);
+      const receipt = await userOpResponse.wait();
+  
+      console.log(`Transaction receipt: ${JSON.stringify(receipt, null, 2)}`);
+      window.alert("Transaction successful!");
+    } catch (error) {
+      console.log(error);
+    }
+  
+    setIsLoading(false);
   }
 
   // Get the address of the smart account when the component loads
